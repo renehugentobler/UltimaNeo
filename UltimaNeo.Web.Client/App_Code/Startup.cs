37b4619e923cc63c777f7d9fd589c6b188ab607e
Rene Hugentobler <rene.hugentobler@gmail.com>
2016-04-14 01:43:42 -0400

    Added Web Client Project

---
+++ UltimaNeo.Web.Client/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UltimaNeo.Web.Client.Startup))]
+namespace UltimaNeo.Web.Client
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
