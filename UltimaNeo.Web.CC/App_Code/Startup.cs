41226d38e6b3e205d606baa056348739efc467af
Rene Hugentobler <rene.hugentobler@gmail.com>
2016-04-14 14:46:03 -0400

    added Web CC project

---
+++ UltimaNeo.Web.CC/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UltimaNeo.Web.CC.Startup))]
+namespace UltimaNeo.Web.CC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
